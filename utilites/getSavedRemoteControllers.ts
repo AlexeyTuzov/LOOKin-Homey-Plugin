7d839fed86a8fd6122641c0083f12c7a71095889
AresNexus <aresnexus@rambler.ru>
2022-02-17 15:35:09 +0500

    fixed unhandled errors in HTTP requests

--- utilites/getSavedRemoteControllers.ts
+++ utilites/getSavedRemoteControllers.ts
@@ -1,29 +1,35 @@
 import * as http from 'http';
-import {RemoteController} from "./interfaces";
+import { RemoteController } from "./interfaces";
 import getDetailedRCInfo from "./getDetailedRCInfo";
 
 const getSavedRemoteControllers = async (IP: string): Promise<RemoteController[]> => {
 
     return new Promise((resolve, reject) => {
 
-        http.get({host: IP, path: '/data'}, res => {
-            let data: string = '';
+        try {
+            http.get({ host: IP, path: '/data' }, res => {
+                let data: string = '';
 
-            res.on('data', chunk => {
-                data += chunk;
+                res.on('data', chunk => {
+                    data += chunk;
+                });
+                res.on('end', async () => {
+                    let result: RemoteController[] = await getDetailedRCInfo(JSON.parse(data), IP);
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(console.log('No saved remotes found in LOOK.in device! Please, add some using LOOK.in app!'));
+                    }
+                });
+                res.on('error', err => {
+                    reject(console.log('Error getting saved remotes from LOOK.in device!', err.stack));
+                });
             });
-            res.on('end', async () => {
-                let result: RemoteController[] = await getDetailedRCInfo(JSON.parse(data), IP);
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject(console.log('No saved remotes found in LOOK.in device! Please, add some using LOOK.in app!'));
-                }
-            });
-            res.on('error', err => {
-                reject(console.log('Error getting saved remotes from LOOK.in device!', err.stack));
-            });
-        });
+        } catch (err: any) {
+            console.log('GET Saved Remote controllers has failed!');
+            reject(console.log('HTTP GET Request Error:', err.stack));
+        }
+
     });
 
 }
--- utilites/httpRequest.ts
+++ utilites/httpRequest.ts
@@ -1,23 +1,28 @@
 import * as http from 'http';
 
-
 const httpRequest = async (IP: string, path: string): Promise<string> => {
 
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-        http.get({host: IP, path: path}, res => {
-            let data: string = '';
-            res.on('data', chunk => {
-                data += chunk;
+        try {
+            http.get({ host: IP, path: path }, res => {
+                let data: string = '';
+                res.on('data', chunk => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    resolve(data);
+                });
+                res.on('error', err => {
+                    reject(console.log(err.stack));
+                });
             });
-            res.on('end', () => {
-                resolve(data);
-            });
-            res.on('error', err => {
-                reject(console.log(err.stack));
-            });
-        });
+        } catch (err: any) {
+            console.log('HTTP Request has failed!');
+            reject(console.log('HTTP GET Request Error:', err.stack));
+        }
     });
+
 }
 
 export default httpRequest;
